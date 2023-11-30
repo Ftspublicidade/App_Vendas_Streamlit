e21278009a1949f92301fa1e9a185a58251dce7d
Fernanda Santos <ftspublicidade@gmail.com>
2023-11-30 18:41:08 -0300

    upload files

---
+++ pages/Acompanhamento_mensal.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+# carregar as bases de dados
+df_vendas = pd.read_excel("Vendas.xlsx")
+df_produtos = pd.read_excel("Produtos.xlsx")
+
+df = pd.merge(df_vendas, df_produtos, how='left', on='ID Produto')
+
+# Criando colunas
+df["Custo"] = df["Custo Unitário"] * df["Quantidade"]
+df["Lucro"] = df["Valor Venda"] - df["Custo"]
+df["mes_ano"] = df["Data Venda"].dt.to_period("M").astype(str)
+
+MoM = df.groupby("mes_ano")["Lucro"].sum().reset_index()
+MoM["Último Mês"] = MoM["Lucro"].shift(1)
+MoM["Variação"] = MoM["Lucro"] - MoM["Último Mês"]
+MoM["Variação%"] = MoM["Variação"] / MoM["Último Mês"] *100
+MoM["Variação%"] = MoM["Variação%"].map('{:.2f}%'.format)
+MoM["Variação%"] = MoM["Variação%"].replace("nan%", "")
+
+
+
+def color_negative(valor):
+    color = "red" if valor < 0 else "black"
+    return f'color: {color}'
+
+def main():
+
+    st.header("Análise Mensal")
+    # Aplicando a função color_negative usando applymap
+    df_styled = MoM.style.format({"Lucro": "R${:20,.2f}", 
+                          "Lucro_LM": "R${:20,.2f}", 
+                          "Variação": "{:20,.2f}"})\
+                 .hide(axis="index")\
+                 .applymap(color_negative, subset=["Variação"])
+
+    st.dataframe(df_styled)
+    
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ vendas.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from streamlit_extras.metric_cards import style_metric_cards 
+
+# carregar as bases de dados
+df_vendas = pd.read_excel("Vendas.xlsx")
+df_produtos = pd.read_excel("Produtos.xlsx")
+
+df = pd.merge(df_vendas, df_produtos, how='left', on='ID Produto')
+
+# Criando colunas
+df["Custo"] = df["Custo Unitário"] * df["Quantidade"]
+df["Lucro"] = df["Valor Venda"] - df["Custo"]
+df["mes_ano"] = df["Data Venda"].dt.to_period("M").astype(str)
+
+#Agrupamentos
+produtos_vendidos_marca = df.groupby("Marca")["Quantidade"].sum().sort_values(ascending=True).reset_index()
+lucro_categoria = df.groupby("Categoria")["Lucro"].sum().reset_index()
+lucro_mes_categoria = df.groupby(["mes_ano", "Categoria"])["Lucro"].sum().reset_index()
+
+def main():
+
+    #st.sidebar.image("f3.png", width=150)
+    st.title("Dasboard de Vendas 📊")
+
+    total_custo = (df["Custo"].sum()).astype(str)
+    total_custo = total_custo.replace(".",",")
+    total_custo = "R$" + total_custo[:2] + "." + total_custo[2:5] + "." + total_custo[5:]
+
+    
+    total_lucro = (df["Lucro"].sum()).astype(str)
+    total_lucro = total_lucro.replace(".",",")
+    total_lucro = "R$" + total_lucro[:2] +"." + total_lucro[2:5] + "." + total_lucro[5:]
+
+    total_clientes = df["ID Cliente"].nunique()
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.metric("Total Custo", total_custo)
+        style_metric_cards(border_left_color="#3e4095")
+
+    with col2:
+        st.metric("Total Lucro", total_lucro)
+
+    with col3:
+        st.metric("Total Clientes", total_clientes)
+
+    st.markdown(
+    """
+    <style>
+    [data-testid="stMetricValue"] {
+        font-size: 18px;
+        color: rgba(0,0,0,0,)
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+    )
+
+    st.markdown(
+    """
+    <style>
+    [data-testid="stMetricLabel"] {
+        font-size: 40px;
+        color: rgba(0,0,0,0,)
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+    )
+
+    st.sidebar.markdown(
+    """
+    <style>
+        .sidebar .sidebar-content {
+            width: 200px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+    col1, col2 = st.columns(2)
+
+    fig = px.bar(produtos_vendidos_marca, x="Quantidade", y="Marca", orientation="h",
+                 text="Quantidade", title="Total produtos vendidos por Marca",
+                 width=400, height=350, color_discrete_sequence=["#3e4095"])
+    fig.update_yaxes(automargin=True)
+    fig.update_layout(title_x=0.5)
+    col1.plotly_chart(fig, theme=None)
+
+    fig1 = px.pie(lucro_categoria, values="Lucro", names="Categoria",
+                  title="Lucro por Categoria", width=400, height=350,
+                  color_discrete_sequence=["#3e4095", "#EC610C"])
+    fig1.update_layout(title_x=0.5)
+    col2.plotly_chart(fig1)
+
+    fig2 = px.line(lucro_mes_categoria, x="mes_ano", y="Lucro", color="Categoria",
+                   title="Lucro x Mês x Categoria", markers=True,
+                   width=700,height=450, color_discrete_sequence=["#3e4095", "#EC610C", "#8860FF"])
+    fig2.update_layout(title_x=0.3)
+    st.plotly_chart(fig2)
+
+
+if __name__ == "__main__":
+    main()
