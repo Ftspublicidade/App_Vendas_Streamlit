72c57926118b82279ed9cce5938f17d3ed6d1df9
Fernanda Santos <ftspublicidade@gmail.com>
2023-12-01 09:56:34 -0300

    Update Acompanhamento_mensal.py

--- pages/Acompanhamento_mensal.py
+++ pages/Acompanhamento_mensal.py
@@ -2,23 +2,21 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-# carregar as bases de dados
-df_vendas = pd.read_excel("Vendas.xlsx")
-df_produtos = pd.read_excel("Produtos.xlsx")
+@st.cache_data
+def carregar_dados():
+    # carregar as bases de dados
+    df_vendas = pd.read_excel("Vendas.xlsx")
+    df_produtos = pd.read_excel("Produtos.xlsx")
 
-df = pd.merge(df_vendas, df_produtos, how='left', on='ID Produto')
+    df = pd.merge(df_vendas, df_produtos, how='left', on='ID Produto')
 
-# Criando colunas
-df["Custo"] = df["Custo Unitário"] * df["Quantidade"]
-df["Lucro"] = df["Valor Venda"] - df["Custo"]
-df["mes_ano"] = df["Data Venda"].dt.to_period("M").astype(str)
+    # Criando colunas
+    df["Custo"] = df["Custo Unitário"] * df["Quantidade"]
+    df["Lucro"] = df["Valor Venda"] - df["Custo"]
+    df["mes_ano"] = df["Data Venda"].dt.to_period("M").astype(str)
+    df["Ano"] = df["Data Venda"].dt.year
 
-MoM = df.groupby("mes_ano")["Lucro"].sum().reset_index()
-MoM["Último Mês"] = MoM["Lucro"].shift(1)
-MoM["Variação"] = MoM["Lucro"] - MoM["Último Mês"]
-MoM["Variação%"] = MoM["Variação"] / MoM["Último Mês"] *100
-MoM["Variação%"] = MoM["Variação%"].map('{:.2f}%'.format)
-MoM["Variação%"] = MoM["Variação%"].replace("nan%", "")
+    return df
 
 
 
@@ -27,6 +25,16 @@ def color_negative(valor):
     return f'color: {color}'
 
 def main():
+
+    df = carregar_dados()
+
+    MoM = df.groupby("mes_ano")["Lucro"].sum().reset_index()
+    MoM["Último Mês"] = MoM["Lucro"].shift(1)
+    MoM["Variação"] = MoM["Lucro"] - MoM["Último Mês"]
+    MoM["Variação%"] = MoM["Variação"] / MoM["Último Mês"] *100
+    MoM["Variação%"] = MoM["Variação%"].map('{:.2f}%'.format)
+    MoM["Variação%"] = MoM["Variação%"].replace("nan%", "")
+
 
     st.header("Análise Mensal")
     # Aplicando a função color_negative usando applymap
